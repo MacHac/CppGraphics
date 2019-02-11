fd1da67e2dd1169f623ba81ccbdfc763a23fb5f3
MacHac <legolicious123@gmail.com>
2019-02-11 16:28:56 -0500

    Corrected error in CMakeLists.txt, started basic GLFW test.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 
-using namespace std;
+/*
+* In the interest of not dumping the entire opengl codebase into std,
+* this file (and any others with graphics code includes) should be unnamespaced.
+*/
+
+void error_callback(int err, const char* desc) {
+    fprintf(stderr, "Error %d: %s\n", err, desc);
+}
 
 int main(void) {
-	cout << "Hello, world!" << endl;
+	std::cout << "Hello, world!" << std::endl;
+	if (!glfwInit())
+	{
+        std::cout << "WHUPS." << std::endl;
+        return 1;
+	}
+	//Set errors to be dumped to stderr
+	glfwSetErrorCallback(error_callback);
+
+	GLFWwindow* win = glfwCreateWindow(640, 480, "m'window", NULL, NULL);
+	if (!win)
+	{
+        std::cout << "window creation failed." << std::endl;
+        return 2;
+	}
+
+	while (!glfwWindowShouldClose(win));
 }
